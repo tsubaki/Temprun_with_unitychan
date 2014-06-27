a9f90c3fd78108b64a7f3f89cda3da7402c9ae1d
tatsuhiko yamamura <tatsuhiko@unity3d.com>
2014-06-27 10:14:47 +0900

    サンプル追加

---
+++ TEMPRUN-master/Assets/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float speed = 6;
+
+	// Update is called once per frame
+	void Update () {
+		rigidbody.MovePosition (transform.position + transform.forward * Time.deltaTime * speed);
+
+		if( Input.GetButtonDown("Fire1")){
+			GetComponent<Animator>().SetTrigger("JUMP");
+		}
+
+		if( Input.GetButtonDown("Fire2")){
+			GetComponent<Animator>().SetTrigger("SLIDE");
+		}
+	}
+
+	void OnTriggerEnter (Collider colider)
+	{
+		var stateInfo = GetComponent<Animator>().GetCurrentAnimatorStateInfo (0);
+		bool isJump = stateInfo.IsName("Base Layer.JUMP00");
+		bool isSlide = stateInfo.IsName("Base Layer.SLIDE00");
+		bool isRun = stateInfo.IsName("Base Layer.RUN00_F");
+
+		bool isHigh = colider.CompareTag("High");
+		bool isLow = colider.CompareTag("Low");
+
+		if( (isRun == true) ||
+		    (isJump == true && isHigh == true) ||
+		    (isSlide == true && isLow == true))
+		{
+			GetComponent<Animator>().SetBool ("DEAD", true);
+			speed = 0;
+		}
+	}
+}
---
+++ TEMPRUN-master/README.md
@@ -0,0 +1,6 @@
+TEMPRUN
+=======
+
+play
+
+http://unity3d-jp.github.io/TEMPRUN/web%20player/
